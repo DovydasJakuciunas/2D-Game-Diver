1e617332f600f572bdd4b8adc88f1272af8be1e1
DovydasJakuciunas <d00244777@student.dkit.ie>
2024-11-04 21:41:50 +0000

    :sparkles: :  Basic Movement
    Created a basic script for the character to be moving up and down.
    Organized the folders to meet the requirement.

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float maxDistance = 5f;  
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        // Record the starting position
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Inputs
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Movement Getter
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0) * speed * Time.deltaTime;
+
+        // Move the Character
+        Vector3 newPosition = transform.position + movement;
+
+        // Check if Characters Within the MaxDistance
+        if (Vector3.Distance(startPosition, newPosition) <= maxDistance)
+        {
+            // Move if Allowed
+            transform.position = newPosition;
+        }
+    }
+}
